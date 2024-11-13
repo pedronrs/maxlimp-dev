07bbe4e26aee577c96fa469a8a21df38a056ae03
GioLimat <123849855+GioLimat@users.noreply.github.com>
2024-11-12 22:43:53 -0300

    Update const.py

--- backend/maxlimp/authentication/const.py
+++ backend/maxlimp/authentication/const.py
@@ -2,5 +2,5 @@ MIN_PASSWORD_LENGTH = 8
 MIN_NAME_LENGTH = 2
 ALGORITHM = 'HS256'
 MAX_AGE_JWT = 60 * 60 * 24 * 120 # 120 dias em segundos
-SUPABASE_URL = "https://vmorglmppqgytzksajfe.supabase.co"
-SUPABASE_KEY =  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZtb3JnbG1wcHFneXR6a3NhamZlIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMDE2ODY5NCwiZXhwIjoyMDQ1NzQ0Njk0fQ.O-JXy95Xar8foPlY0mk8MxSs2fnxm4_wGapxm3GLeoo"
+SUPABASE_URL = ""
+SUPABASE_KEY =  ""
